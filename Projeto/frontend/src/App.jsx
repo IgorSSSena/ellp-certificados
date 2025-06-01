bf4effb0606b04ac54ffbe55dd0260b49ef70961
IgorSSSena <igorssena22@gmail.com>
2025-06-01 20:03:41 -0300

    🐛 :Adicao da rota segura entre adm login e userpage

--- Projeto/frontend/src/App.jsx
+++ Projeto/frontend/src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import UserPage from './pages/UserPage';
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-
+import UserPage from './pages/UserPage';
+import AdminPage from './pages/AdminPage';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    <div id='AppContainer'>
-      <Login />
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
 
+        {/* Rotas protegidas por token */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/usuario" element={<UserPage />} />
+        </Route>
+
+        {/* Apenas admin pode acessar */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/admin" element={<AdminPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
